f0f6d576e80a354beb0237c3393961da99d7cb69
devojyotimisra <devojyotimisra1@gmail.com>
2026-02-04 19:06:57 +0530

    Added Content

--- week2/q1/StudentGradingSystem.java
+++ week2/q1/StudentGradingSystem.java
@@ -6,3 +6,107 @@
 // Implement both B.E and M.E students. 
 // B.E. marks = Midterm(15) + Assignment&Attendance(15) + Endterm(70).  
 // M.E. marks = Assignment&Attendance(30) + Endterm(70)
+
+import java.util.*;
+
+class StudentGradingSystem
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter Student Type (BE or ME): ");
+        String studentType = sc.nextLine().trim().toUpperCase();
+
+        System.out.print("Enter Number of Subjects: ");
+        int totalSubjects = sc.nextInt();
+        
+        for(int i=1; i<=totalSubjects; i++)
+        {
+            System.out.println("\nSubject " + i);
+            System.out.print("Subject Type (Enter T for Theory, P for Practical): ");
+            String subType = sc.next().trim().toUpperCase();
+            
+            float marks = 0;
+            
+            if (studentType.equals("BE")) 
+            {
+                 System.out.print("Enter Midterm Marks (max 15): ");
+                 float mid = sc.nextFloat();
+                 System.out.print("Enter Assignment & Attendance Marks (max 15): ");
+                 float assig = sc.nextFloat();
+                 System.out.print("Enter Endterm Marks (max 70): ");
+                 float end = sc.nextFloat();
+                 marks = mid + assig + end;
+            } 
+            else 
+            {
+                 System.out.print("Enter Assignment & Attendance Marks (max 30): ");
+                 float assig = sc.nextFloat();
+                 System.out.print("Enter Endterm Marks (max 70): ");
+                 float end = sc.nextFloat();
+                 marks = assig + end;
+            }
+            
+            boolean isPassed = false;
+
+            if (subType.equals("P")) 
+            {
+                if (marks >= 50) 
+                {
+                        isPassed = true;
+                }
+            } 
+            else 
+            {
+                if (marks >= 40)
+                {
+                    isPassed = true;
+                }
+            }
+            
+            String grade;
+
+            if(marks >= 90)
+            {
+                grade = "EX";
+            }
+            else if(marks >= 80)
+            {
+                grade = "O";
+            }
+            else if(marks >= 70)
+            {
+                grade = "A";
+            }
+            else if(marks >= 60)
+            {
+                grade = "B";
+            }
+            else if(marks >= 50)
+            {
+                grade = "C";
+            }
+            else if(marks>= 40)
+            {
+                if(isPassed)
+                {
+                    grade = "D";
+                }
+                else
+                {
+                    grade = "F";
+                }
+            }
+            else
+            {
+                grade = "F";
+            }
+            
+            System.out.println("Total Marks: " + marks);
+            System.out.println("Grade: " + grade);
+            System.out.println("Result: " + (isPassed ? "PASS" : "FAIL"));
+        }
+        sc.close();
+    }
+}
