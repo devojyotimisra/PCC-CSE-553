a763c5f0d4fa7a4b2374ab0928ced1f0350298ee
devojyotimisra <devojyotimisra1@gmail.com>
2026-02-04 13:22:00 +0530

    Added Content

---
+++ Week 2/ShapeDrawer.java
@@ -0,0 +1,2 @@
+// Square and Circles:
+// Draw the number of squares or circles the user inputs to draw, in GUI
---
+++ Week 2/StudentGradingSystem.java
@@ -0,0 +1,8 @@
+// Student Grading System: 
+// Display the grade of each student along with pass/fail of each subject. 
+// Take necessary input from the user. 
+// Theory subject passing criteria : Marks >= 40. 
+// Practical subject passing criteria : Marks >= 50. 
+// Implement both B.E and M.E students. 
+// B.E. marks = Midterm(15) + Assignment&Attendance(15) + Endterm(70).  
+// M.E. marks = Assignment&Attendance(30) + Endterm(70)
