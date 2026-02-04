b4586cac18d5b354a9e8c03e490e73aaf49fdc16
devojyotimisra <devojyotimisra1@gmail.com>
2026-02-04 16:02:10 +0530

    Added Content

--- Week 3/Threads.java
+++ Week 3/Threads.java
@@ -1,1 +1,63 @@
 // Write a Java program to implement various functionalities of Threads.
+
+class MyThreadOne extends Thread
+{
+    public void run()
+    {
+        for(int i=0; i<100; i++)
+        {
+            System.out.println("Hi");
+            try
+            {
+                Thread.sleep(100);
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+class MyThreadTwo extends Thread
+{
+    public void run()
+    {
+        for(int i=0; i<100; i++)
+        {
+            System.out.println("Hello");
+            try
+            {
+                Thread.sleep(100);
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+class Threads
+{
+    public static void main()
+    {
+        Thread t1 = new MyThreadOne();
+        Thread t2 = new MyThreadTwo();
+
+        t1.start();
+        t2.start();
+
+        try
+        {
+            t1.join();
+            t2.join();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+        System.out.println("Finished");
+    }
+}
